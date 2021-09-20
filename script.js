ce8408a2e8285afd06520d9e204a5fd9504c7834
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-09-20 22:43:04 +0930

    created function that prompts user to choose length and characters for password

--- script.js
+++ script.js
@@ -5,16 +5,42 @@ var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var characters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '/', '<', '>', '?'];
 
 
-
-
-
-
+// generate password button
 var generateBtn = document.querySelector("#generate");
+//function requires user input before a new password is generated.
+  function generatePassword() {
+    //when user clicks generate password button, a window will appear and prompt them to choose a password length between two numbers. 
+    var length = parseInt(window.prompt("Welcome to my password generator! Please enter a password length between 8 and 128 characters to continue:"));
+    //if the user chooses a length lower than 8 or higher than 128, they will be alerted that the number is invalid and that they must pick a number between said lengths.
+    if (length < 8||length > 128) {
+      alert("Invalid: Please enter a password length between 8 and 128 characters to continue:");
+      return;
+    }
+  
+  //User will be asked whether they would like lowercase/uppercase letters, numbers or characters in their password. User must click 'ok' or 'cancel' to each of the following prompts in order to move forward.
+  var inputLowercase = confirm("Would you like your new password to contain lowercase letters?");
+  var inputUppercase = confirm("Would you like your new password to contain uppercase letters?");
+  var inputNumeric = confirm("Would you like your new password to contain numbers?");
+  var inputCharacters = confirm("Would you like your new password to contain special characters?");
 
-function generatePassword() {
-  return ""
+  //if user clicks 'cancel' to all of the above than an alert will appear letting them know they must choose one of the criteria to move forward. 
+  if (!inputLowercase && !inputUppercase && !inputNumeric && !inputCharacters) {
+    alert("Invalid: You must choose at least one option to meet minimum criteria for password to be generated!");
+    return;
+  }
+  var userChoice = {
+    length: length,
+    inputLowercase: inputLowercase,
+    inputUppercase: inputUppercase,
+    inputNumeric: inputNumeric,
+    inputCharacters: inputCharacters,
+  }
+  return userChoice;
 }
 
+
+
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
