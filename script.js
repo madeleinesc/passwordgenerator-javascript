e16017a4068229493404899f67d86a2c6b82e265
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-09-20 19:29:32 +0930

    created letter, number and character arrays

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// Assignment Code
+// arrays of possible letter/numeric/character choices for password generator to pick from:
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var characters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '/', '<', '>', '?'];
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
